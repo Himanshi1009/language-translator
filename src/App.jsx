3340e5a9c5e1060037675ba4ba55b06ed0120edb
himanshi <hggupta.2004@gmail.com>
2024-06-27 21:53:31 +0530

    translator

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Body from './Body'
+
+function App() {
+
+  return (
+    <div className=' bg-blue-300 h-[775px] w-full items-center flex '>
+      <Body/>
+
+    </div>
+  )
+}
+
+export default App
